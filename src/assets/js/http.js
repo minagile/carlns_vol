1700769cd963d813b0e39b10e1071654cd04462b
XianQijing <154122662@qq.com>
2018-12-17 12:06:30 +0800

    11

--- src/assets/js/http.js
+++ src/assets/js/http.js
@@ -6,10 +6,10 @@ import { Message } from 'element-ui'
 axios.defaults.timeout = 10000
 // axios.defaults.baseURL = 'http://192.168.1.128:80' // 彭
 // axios.defaults.baseURL = 'http://192.168.1.145:80'
-// axios.defaults.baseURL = 'http://192.168.1.140:80'
+axios.defaults.baseURL = 'http://192.168.1.140:80'
 // axios.defaults.baseURL = 'http://192.168.1.102:8848'
 // axios.defaults.baseURL = 'http://192.168.1.136:8848'
-axios.defaults.baseURL = 'http://192.168.1.145:8848'
+// axios.defaults.baseURL = 'http://192.168.1.145:8848'
 // axios.defaults.baseURL = 'http://192.168.1.117:80'
 // axios.defaults.baseURL = 'http://192.168.1.136:80'
 // axios.defaults.baseURL = 'https://www.easyfq.com'
--- src/components/Vol/Decision/Calculater.vue
+++ src/components/Vol/Decision/Calculater.vue
@@ -4,9 +4,11 @@
     <p class="title">车险计算器</p>
       <div style="display: flex;justify-content: space-between;">
     <div class="content">
-      <div class="select">
-        <span>选择车险</span>
-        <button class="add" @click="add">添加</button>
+      <div>
+        <div class="select">
+          <span>手动输入</span>
+          <button class="add" @click="add">添加</button>
+        </div>
       </div>
       <div style="height:99%;overflow: auto;">
         <div class="table" v-for="(item, index) in list" :key="index">
@@ -43,23 +45,87 @@
             </el-date-picker>
         </div>
         <button class="find" @click="getData">查询</button>
+        <div>
+          <div class="select">
+            <span>显示结果</span>
+          </div>
+          <div style="height:99%;overflow: auto;" v-if="datalist.length > 0">
+            <div class="table qq" style="border: 1px solid black;" v-for="(tab, i) in datalist" :key="i">
+              <div style="margin-left:10px;">总收入：{{tab.sf}}</div>
+              <div>盈利：{{tab.yl}}</div>
+              <div>本金：{{tab.bj}}</div>
+              <div>我方垫付：{{tab.jf}}</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <div style="width:40%;margin-left:5px;">
+    <div style="width:50%;margin-left:20px;">
       <div class="content ss">
         <div class="select">
-        <span>显示结果</span>
-      </div>
-        <div style="height:99%;overflow: auto;" v-if="datalist.length > 0">
-          <div class="table" v-for="(tab, i) in datalist" :key="i">
-            <div style="margin-left:10px;">总收入：{{tab.sf}}</div>
-            <div>盈利：{{tab.yl}}</div>
-            <div>本金：{{tab.bj}}</div>
-            <div>我方垫付：{{tab.jf}}</div>
+          <span>自动选择</span>
+        </div>
+        <div class="selectData">
+          <div style="display: flex;justify-content: space-between;">
+            <el-cascader @visible-change="select"
+              :options="options2"
+              @change="changechan"
+              :show-all-levels="false"
+              @active-item-change="handleItemChange"
+              clearable
+              :props="props"
+            ></el-cascader>
+            <el-select v-model="value" placeholder="请选择" style="width: 220px;">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+            <el-date-picker
+              v-model="timeType"
+              type="date"
+              placeholder="选择日期"
+              value-format="yyyy-MM-dd">
+            </el-date-picker>
+          </div>
+          <div class="findbutton">
+            <button class="find1" @click="search2">查询</button>
           </div>
         </div>
         <div class="xianshi">
-          <table>
+          <table v-if="show8">
+            <tr>
+              <th></th>
+              <th>总收入</th>
+              <th>盈利</th>
+              <th>本金</th>
+              <th>我方垫付</th>
+            </tr>
+            <tr>
+              <th>统计商业险</th>
+              <td>{{resSoure.commercials.sfs}}</td>
+              <td>{{resSoure.commercials.yls}}</td>
+              <td>{{resSoure.commercials.bjs}}</td>
+              <td>{{resSoure.commercials.jfs}}</td>
+            </tr>
+            <tr>
+              <th>统计交强险</th>
+              <td>{{resSoure.sali.sfc}}</td>
+              <td>{{resSoure.sali.ylc}}</td>
+              <td>{{resSoure.sali.bjc}}</td>
+              <td>{{resSoure.sali.jfc}}</td>
+            </tr>
+            <tr>
+              <th>合计</th>
+              <td>{{resSoure.total.a}}</td>
+              <td>{{resSoure.total.b}}</td>
+              <td>{{resSoure.total.c}}</td>
+              <td>{{resSoure.total.d}}</td>
+            </tr>
+          </table>
+          <table v-if="!show8">
             <tr>
               <th></th>
               <th>总收入</th>
@@ -143,7 +209,54 @@ export default {
       ],
       listNum: 0,
       es: [],
-      datalist: []
+      datalist: [
+        {
+          sf: 0,
+          yl: 0,
+          bj: 0,
+          jf: 0
+        }
+      ],
+      value: '',
+      options2: [],
+      options: [
+        {
+          value: 0,
+          label: '综合险'
+        },
+        {
+          value: 1,
+          label: '商业险'
+        },
+        {
+          value: 2,
+          label: '交强险'
+        }
+      ],
+      props: {
+        // value: 'label',
+        label: 'label',
+        value: 'value',
+        children: 'cities'
+      },
+      channelId: '',
+      timeType: '',
+      show8: true,
+      tableList2: {
+        advances: 0,
+        cAdvances: 0,
+        cIncome: 0,
+        cPrincipal: 0,
+        cProfit: 0,
+        cTrueIncome: 0,
+        cTrueProfit: 0,
+        income: 0,
+        principal: 0,
+        profit: 0,
+        trueIncome: 0,
+        trueProfit: 0,
+        type: 0
+      }
     }
   },
   methods: {
@@ -177,10 +290,74 @@ export default {
         if (res.data.code === 0) {
           this.datalist = res.data.data1
           this.resSoure = res.data.data2
-          console.log(this.resSoure.commercials.sfs)
+          // console.log(this.resSoure.commercials.sfs)
         } else {
           this.$message(res.data.msg)
         }
+      })
+    },
+    handleItemChange (val) {
+      // console.log(val)
+      setTimeout(_ => {
+        var id = ''
+        // POST /admin/channel/getNextChannel
+        this.options2.forEach((v, k) => {
+          if (v.value === val[0]) {
+            id = v.value
+            this.$post('/admin/channel/getNextChannel', {
+              parentId: id
+            }).then(res => {
+              // console.log(res)
+              if (res.code === 0) {
+                v.cities = []
+                if (res.data.length > 0) {
+                  v.cities = [{ label: v.label, value: v.value }]
+                  res.data.forEach(m => {
+                    v.cities.push({ label: m.channelName, value: m.channelId })
+                  })
+                } else {
+                  v.cities.push({ label: v.label, value: v.value })
+                }
+              }
+            })
+          }
+        })
+      }, 300)
+    },
+    // 查询所有渠道
+    select (val) {
+      if (val === true) {
+        // this.selectAllChannel = []
+        this.options2 = []
+        // GET /admin/channel/getOneChannel
+        this.$fetch('/admin/channel/getOneChannel').then(res => {
+          // console.log(res.data)
+          if (res.code === 0) {
+            res.data.forEach(v => {
+              this.options2.push({value: v.channelId, label: v.channelName, cities: []})
+              // this.selectAllChannel.push({value: v.channelId, label: v.channelName})
+            })
+          } else {
+            this.$message(res.msg)
+          }
+        })
+      }
+    },
+    changechan (val) {
+      console.log(val)
+      // console.log(Array.reverse(val))
+      this.channelId = val[val.length - 1]
+      this.allchannel = true
+      // this.getData()
+    },
+    search2 () {
+      this.$post('/interestRate/calculator', {
+        type: this.value,
+        time: this.timeType,
+        channelId: this.channelId
+      }).then(res => {
+        this.show8 = false
+        this.tableList2 = res.data
       })
     }
   }
@@ -198,7 +375,7 @@ export default {
   padding: 40px;
   overflow: scroll;
   font-size: 9px;
-  // background-image: url('../../../assets/img/jisuan.png');
+  box-sizing: border-box;
   .title {
     font-size: 27px;
     font-weight: bold;
@@ -309,6 +486,8 @@ export default {
     background: white;
     box-shadow:0px 15px 20px 0px rgba(23,3,79,0.1);
     border-radius:20px;
+    margin-top: 20px;
+    border: 1px solid black;
     table {
       // background: white;
       width: 100%;
@@ -317,6 +496,29 @@ export default {
         text-align: center;
         height: 20px;
       }
+    }
+  }
+  .qq {
+    display: flex;
+    justify-content: space-between;
+  }
+  .findbutton {
+    width: 100%;
+    text-align: right;
+    .find1 {
+      width: 113px;
+      height: 39px;
+      font-size:17px;
+      font-family:Adobe Heiti Std R;
+      font-weight:normal;
+      background: #FFC107;
+      border-radius:11px;
+      margin-top: 10px;
+    }
+  }
+  .selectData {
+    .el-date-picker {
+      height: 40px;;
     }
   }
 </style>

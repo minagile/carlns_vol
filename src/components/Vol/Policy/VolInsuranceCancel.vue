bf44e6ddb62a859333b98958766cc901dfd68ea2
XianQijing <154122662@qq.com>
2018-12-19 10:44:31 +0800

    'ff'

--- src/components/Vol/Policy/VolInsuranceCancel.vue
+++ src/components/Vol/Policy/VolInsuranceCancel.vue
@@ -30,6 +30,11 @@
           {{ scope.row.createTime | timeChange }}
         </template>
       </el-table-column>
+      <el-table-column label="退保时间" width="120">
+        <template slot-scope="scope">
+          {{ scope.row.updateTime | timeChange }}
+        </template>
+      </el-table-column>
       <el-table-column prop="remark" label="退保原因"></el-table-column>
       <el-table-column width="80">
         <template slot-scope="scope">
